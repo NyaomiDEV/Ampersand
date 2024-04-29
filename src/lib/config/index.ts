83fb72fdf433edc7e99f6ac1ca397cbe8846dad7
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-04-29 18:50:31 +0200

    update thing

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,27 +1,25 @@
 import { getCompound, setCompound } from "../util/localstorage";
 import { Config } from "./types";
 
-export function getDefaultConfig(): Config {
-	return {
-		app: {
-			language: "en",
-			view: "dashboard",
-			filterQueryMembers: "",
-			filterQueryJournal: ""
-		},
-		accessibility: {
-			highLegibility: false,
-			theme: "auto",
-			accentColor: undefined,
-			reducedMotion: false,
-			uiScale: 1
-		},
-		security: {
-			pinCode: undefined,
-			useBiometrics: false
-		}
+export const defaultConfig: Config = {
+	app: {
+		language: "en",
+		view: "dashboard",
+		filterQueryMembers: "",
+		filterQueryJournal: ""
+	},
+	accessibility: {
+		highLegibility: false,
+		theme: "auto",
+		accentColor: undefined,
+		reducedMotion: false,
+		uiScale: 1
+	},
+	security: {
+		pinCode: undefined,
+		useBiometrics: false
 	}
-}
+};
 
 export function getConfig(): Config {
 	return getCompound("config");
