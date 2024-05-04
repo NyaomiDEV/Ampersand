906c71c9780cf18120949681e2be3e745d9f4fd9
Cat-a-clysm <38079631+mecha-cat@users.noreply.github.com>
2024-05-05 00:00:05 +0200

    More work on the options page

--- src/router/settings.ts
+++ src/router/settings.ts
@@ -2,6 +2,36 @@ import { RouteRecordRaw } from "vue-router";
 
 const settings: Array<RouteRecordRaw> = [
 	{
+		path: '/options/system',
+		name: 'System',
+		component: (await import("../views/options/System.vue")).default,
+	},
+	{
+		path: '/options/front_history',
+		name: 'Front History',
+		component: (await import("../views/options/FrontHistory.vue")).default,
+	},
+	{
+		path: '/options/message_board',
+		name: 'Message Board',
+		component: (await import("../views/options/MessageBoard.vue")).default,
+	},
+	{
+		path: '/options/tag_management',
+		name: 'Message Board',
+		component: (await import("../views/options/TagManagement.vue")).default,
+	},
+	{
+		path: '/options/reminders',
+		name: 'Reminders',
+		component: (await import("../views/options/Reminders.vue")).default,
+	},
+	{
+		path: '/options/accessibility',
+		name: 'Accessibility',
+		component: (await import("../views/options/Accessibility.vue")).default,
+	},
+	{
 		path: '/options/about',
 		name: 'About',
 		component: () => import("../views/options/About.vue"),
---
+++ src/views/options/System.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+	import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, IonNavLink, IonBackButton} from '@ionic/vue';
+	import { computed } from 'vue';
+	import { isIOSMode } from '../../lib/util/misc';
+
+	const isIOS = computed(isIOSMode);
+</script>
+
+<template>
+	<IonPage>
+		<IonHeader>
+			<IonToolbar>
+				<IonButton slot="start">
+					<IonBackButton />
+				</IonButton>
+				<IonTitle>
+					System name and photo
+				</IonTitle>
+			</IonToolbar>
+		</IonHeader>
+		
+		<IonContent>
+			<IonList :inset="isIOS">
+
+			</IonList>
+		</IonContent>
+	</IonPage>
+</template>
