634fc8d0129c3f09af96f7e3ec44d0e58b579d0d
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-05-05 00:26:55 +0200

    fix minor derp

--- src/views/options/FrontHistory.vue
+++ src/views/options/FrontHistory.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, IonNavLink, IonBackButton} from '@ionic/vue';
+	import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, IonButton, IonBackButton} from '@ionic/vue';
 	import { computed } from 'vue';
 	import { isIOSIonicMode } from '../../lib/mode';
 
