bee503ecdf2a8723433b456cd52aa9b18386cf2f
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-05-04 22:43:03 +0200

    set esnext as target - we don't care about older browsers

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext"
+  },
   plugins: [vue()],
   base: "/Ampersand/" // for now
 })
