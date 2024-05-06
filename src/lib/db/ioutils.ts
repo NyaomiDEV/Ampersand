05417ca1b8bac6146e2f43de803af80572c4a447
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-05-06 17:14:49 +0200

    import/export with password

--- src/lib/db/ioutils.ts
+++ src/lib/db/ioutils.ts
@@ -3,6 +3,7 @@ import { db } from ".";
 import { blob, file, filelist, imagebitmap, imagedata, map, set, typedArrays, undef } from "typeson-registry";
 import { decode, encode } from "@msgpack/msgpack";
 import { compressGzip, decompressGzip } from "../util/misc";
+import { EncryptedPayload, decrypt, encrypt } from "../util/crypto";
 
 const typeson = new Typeson({
 	sync: false,
@@ -19,26 +20,50 @@ const typeson = new Typeson({
 	imagedata
 ]);
 
-export async function exportDatabaseToBinary(){
+async function _exportDatabase(){
 	const theEntireDatabase: Record<string, any> = {};
-	for(const x of db.tables){
+	for (const x of db.tables) {
 		theEntireDatabase[x.name] = await typeson.encapsulate(await x.toArray());
 	}
+
+	return theEntireDatabase;
+}
+
+export async function exportDatabaseToBinary(){
+	const theEntireDatabase = await _exportDatabase();
 
 	return await compressGzip(encode(theEntireDatabase));
 }
 
-export async function importDatabaseFromBinary(data: Uint8Array) {
-	const theEntireDatabase = decode(await decompressGzip(data)) as Record<string, any>;
+export async function exportDatabaseToBinaryWithPassword(password: string){
+	const theEntireDatabase = await _exportDatabase();
+	const encodedDatabase = await encrypt(encode(theEntireDatabase), password);
 
-	for(const key of Object.getOwnPropertyNames(theEntireDatabase)){
+	return await compressGzip(encode(encodedDatabase));
+}
+
+async function _importDatabase(theEntireDatabase: Record<string, any>){
+	for (const key of Object.getOwnPropertyNames(theEntireDatabase)) {
 		const table = db.tables.find((x) => x.name === key);
-		if(table){
+		if (table) {
 			const contents = await typeson.revive(theEntireDatabase[key]);
 			await table.clear();
 			await table.bulkAdd(contents);
 		}
 	}
+}
+
+export async function importDatabaseFromBinary(data: Uint8Array) {
+	const theEntireDatabase = decode(await decompressGzip(data)) as Record<string, any>;
+
+	return await _importDatabase(theEntireDatabase);
+}
+
+export async function importDatabaseFromBinaryWithPassword(data: Uint8Array, password: string) {
+	const encodedDatabase = decode(await decompressGzip(data)) as EncryptedPayload;
+	const theEntireDatabase = decode(await decrypt(encodedDatabase, password)) as Record<string, any>;
+
+	return await _importDatabase(theEntireDatabase);
 }
 
 export async function exportTableToBinary(tableName: string){
