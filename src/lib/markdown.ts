430b0ed1b8755546b2a327cafab05acae4113754
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-07-29 01:47:12 +0200

    update dependencies
    wtf happened to unifiedjs

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -5,8 +5,10 @@ import { unified } from 'unified';
 
 export function getMarkdownFor(text: string) {
 	return unified()
+		// @ts-expect-error
 		.use(remarkParse)
 		.use(remarkRehype)
+		// @ts-expect-error
 		.use(rehypeStringify)
 		.processSync(text).toString()
 }
