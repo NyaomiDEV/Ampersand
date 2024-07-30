5234e3a048651d20fb6ca7eb7d5e4f7f52f46b0a
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-07-30 18:02:01 +0200

    fix build

--- src/views/tabbed/TestingGrounds.vue
+++ src/views/tabbed/TestingGrounds.vue
@@ -2,45 +2,21 @@
 	import {
 		IonContent,
 		IonHeader,
-		IonInput,
 		IonItem,
 		IonLabel,
 		IonList,
-		IonListHeader,
 		IonPage,
-		IonTextarea,
 		IonTitle,
 		IonToolbar
 	} from '@ionic/vue';
-	import { inject, ref } from 'vue';
-	import { getFiles } from '../../lib/util/misc';
-	import { newMember } from '../../lib/db/entities/members';
-	import { resizeImage } from '../../lib/util/image';
+	import { inject } from 'vue';
 	import { newSystem, getSystemUUID } from '../../lib/db/entities/system';
 
 	const isIOS = inject<boolean>("isIOS");
 
-	const name = ref("");
-	const pronouns = ref("");
-	const role = ref("");
-	const description = ref("");
-
 	async function createTestSystem(){
 		await newSystem({
 			name: "Test System"
-		});
-	}
-
-	async function addToDatabase(){
-		const files = await getFiles();
-		newMember({
-			name: name.value,
-			image: await resizeImage(files[0]!)!,
-			role: role.value,
-			pronouns: pronouns.value,
-			description: description.value,
-			isArchived: false,
-			isCustomFront: false
 		});
 	}
 </script>
