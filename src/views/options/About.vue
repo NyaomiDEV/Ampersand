5115478c5ba484ec36e9e1359db1679ec0c89033
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-07-31 16:34:52 +0200

    polish about section

--- src/views/options/About.vue
+++ src/views/options/About.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-	import { IonContent, IonHeader, IonList, IonImg, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonBackButton} from '@ionic/vue';
-	import { inject } from 'vue';
+	import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonBackButton } from '@ionic/vue';
 
-	import AmpersandLogo from  "../../../assets/ampersand_logo.svg"
-	import DiscordLogo from  "../../../assets/discord_logo.svg";
-	import GithubLogo from  "../../../assets/github_logo.svg";
-	const isIOS = inject<boolean>("isIOS");
+	import AmpersandLogo from  "../../assets/ampersand_logo.svg"
+	import DiscordLogo from  "../../assets/discord_logo.svg";
+	import GithubLogo from  "../../assets/github_logo.svg";
 </script>
 
 <template>
@@ -20,26 +18,47 @@
 		</IonHeader>
 		
 		<IonContent>
-			<IonList :inset="isIOS">				
-				<div class="centered-text">
-					<h1>Ampersand</h1>
-					<p> {{ $t("options:about.version") }} 0.0.0 </p>
-					<p> {{ $t("options:about.madein") }}</p>
+			<div class="container">
+				<h1>Ampersand</h1>
+				<p> {{ $t("options:about.version") }} 0.0.0 </p>
+				<p> {{ $t("options:about.madein") }}</p>
 
-					<IonIcon class="logo" :icon="AmpersandLogo"/>
+				<IonIcon class="logo" :icon="AmpersandLogo" />
 
-					<br />
-					<br />
-
+				<div class="buttons">
 					<IonButton shape="round" href="https://discord.com/invite/xCptGJKeKc">
-						<IonIcon slot="icon-only" :icon="DiscordLogo" />
+						<IonIcon slot="icon-only" :icon="DiscordLogo"></IonIcon>
 					</IonButton>
 
 					<IonButton shape="round" href="https://github.com/NyaomiDEV/Ampersand">
-						<IonIcon slot="icon-only" :icon="GithubLogo" />
+						<IonIcon slot="icon-only" :icon="GithubLogo" ></IonIcon>
 					</IonButton>
 				</div>
-			</IonList>
+			</div>
 		</IonContent>
 	</IonPage>
 </template>
+
+<style scoped>
+	.container {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	p {
+		margin: 0;
+	}
+
+	.logo {
+		margin: 32px 0;
+	}
+
+	.buttons {
+		display: flex;
+		gap: 16px;
+	}
+</style>
