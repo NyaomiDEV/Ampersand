97e8fabeb0b7f2ea7317a436ae2af0e7e8e1a3ea
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-07-31 17:50:31 +0200

    use version from package json

--- src/views/options/About.vue
+++ src/views/options/About.vue
@@ -4,6 +4,8 @@
 	import AmpersandLogo from  "../../assets/ampersand_logo.svg"
 	import DiscordLogo from  "../../assets/discord_logo.svg";
 	import GithubLogo from  "../../assets/github_logo.svg";
+
+	import { version } from "../../../package.json";
 </script>
 
 <template>
@@ -20,17 +22,17 @@
 		<IonContent>
 			<div class="container">
 				<h1>Ampersand</h1>
-				<p> {{ $t("options:about.version") }} 0.0.0 </p>
+				<p> {{ $t("options:about.version", { version }) }}</p>
 				<p> {{ $t("options:about.madein") }}</p>
 
 				<IonIcon class="logo" :icon="AmpersandLogo" />
 
 				<div class="buttons">
-					<IonButton shape="round" href="https://discord.com/invite/xCptGJKeKc">
+					<IonButton shape="round" href="https://discord.com/invite/xCptGJKeKc" target="_blank">
 						<IonIcon slot="icon-only" :icon="DiscordLogo"></IonIcon>
 					</IonButton>
 
-					<IonButton shape="round" href="https://github.com/NyaomiDEV/Ampersand">
+					<IonButton shape="round" href="https://github.com/NyaomiDEV/Ampersand" target="_blank">
 						<IonIcon slot="icon-only" :icon="GithubLogo" ></IonIcon>
 					</IonButton>
 				</div>
