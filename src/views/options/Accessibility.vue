34bc37afc92792096ee26eca79e9f991fa6b0545
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-07-31 16:34:38 +0200

    move assets inside src, define classes for dyslexic font

--- src/views/options/Accessibility.vue
+++ src/views/options/Accessibility.vue
@@ -96,6 +96,8 @@
 							:step="0.1"
 							:snaps="true"
 							:ticks="true"
+							:pin="true"
+							:pinFormatter="(v) => `${v}`"
 						/>
 					</IonLabel>
 				</IonItem>
@@ -109,7 +111,9 @@
 							:max="1.5"
 							:step="0.1"
 							:snaps="true"
-							:ticks="false"
+							:ticks="true"
+							:pin="true"
+							:pinFormatter="(v) => `${v}`"
 						/>
 					</IonLabel>
 				</IonItem>
