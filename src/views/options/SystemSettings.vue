a0c61c01edbbc96d70f8d33883265eafcb0c64f5
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-01-10 13:45:58 +0100

    remove unused import lol what [skip ci]

--- src/views/options/SystemSettings.vue
+++ src/views/options/SystemSettings.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 	import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, IonBackButton, IonAvatar, IonButton, IonIcon, IonInput, IonFab, IonFabButton, IonItem, useIonRouter, IonLabel, IonTextarea} from '@ionic/vue';
-	import { inject, onMounted, ref, shallowRef } from 'vue';
+	import { inject, onMounted, ref } from 'vue';
 	import { getBlobURL } from '../../lib/util/blob';
 	import { getFiles } from '../../lib/util/misc';
 	import { resizeImage } from '../../lib/util/image';
