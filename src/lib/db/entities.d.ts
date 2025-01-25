5c396012d357b03c757d16edd2dcd0d8289520b6
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-01-25 19:39:53 +0100

    painful realization moment

--- src/lib/db/entities.d.ts
+++ src/lib/db/entities.d.ts
@@ -85,11 +85,29 @@ interface EventReminder extends ReminderBase {
 	},
 
 	// make it either-or in regards to PeriodicReminder
-	scheduleEveryInterval?: never,
-	scheduleEveryWeekday?: never,
-	scheduleTimeAt?: never
+	scheduleEveryDateInterval?: never,
+	scheduleEveryWeekdayInterval?: never,
+	scheduleTimeInterval?: never
 }
 
+/*
+	periodic reminders are strange in how they work.
+	let's suppose you want a notification every day at 7.30:
+	you'd set the minutes at 30, the hours at 7
+	but you shall not set the day, as an undefined value means "it will repeat every day".
+	once you set a day, it will repeat every month on that day,
+	and by this fashion, if you set a month it will repeat every year that month,
+	that day, so on and so forth.
+
+	to keep this consistent and simple, seconds will be always set to zero,
+	the hours and minutes will always be asked.
+
+	the frontend should then ask "every day" or "set a day in the month"
+	and so on and so forth, "every month" or "set a month in the year"...
+
+	the problem is that we can't do stuff "every two days, every three days" in this way,
+	and Tauri doesn't give us any better ways, sadly.
+*/
 interface PeriodicReminderBase extends ReminderBase {
 	type: "periodic",
 	scheduleTimeInterval?: {
@@ -103,7 +121,7 @@ interface PeriodicReminderBase extends ReminderBase {
 	delay?: never
 }
 
-interface PeriodicReminderDate extends ReminderBase {
+interface PeriodicReminderDate extends PeriodicReminderBase {
 	scheduleEveryDateInterval?: {
 		year?: number,
 		month?: number,
@@ -111,7 +129,7 @@ interface PeriodicReminderDate extends ReminderBase {
 	},
 	scheduleEveryWeekdayInterval?: never
 }
-interface PeriodicReminderWeekday extends ReminderBase {
+interface PeriodicReminderWeekday extends PeriodicReminderBase {
 	scheduleEveryWeekdayInterval?: {
 		monday?: boolean,
 		tuesday?: boolean,
