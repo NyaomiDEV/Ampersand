b2a04db9aee47007e2621bbb22ebd23641c35593
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-01-27 18:12:06 +0100

    more version changes, put the version number in the webapp as well

--- upgrade-vcs.mjs
+++ upgrade-vcs.mjs
@@ -6,6 +6,9 @@ import { spawn } from "node:child_process";
 import { resolve } from "node:path";
 import * as TOML from "smol-toml";
 
+//
+// UTILITY FUNCTIONS
+//
 function spawnAsync(cmd, args, cwd){
 	return new Promise(resolve => {
 		const _process = spawn(cmd, args, {stdio: "pipe", cwd});
@@ -25,29 +28,40 @@ function spawnAsync(cmd, args, cwd){
 	})
 }
 
-async function main(){
+async function getVersion(){
+	// support specifying a version so that we can easily upgrade
+	if(process.argv[2])
+		return process.argv[2];
+
 	const revcount = Number((await spawnAsync("git", ["rev-list", "--count", "HEAD"], import.meta.dirname)).stdout);
-
-	if(revcount){
-		console.log("Revision count is", revcount);
-		const packageJson = JSON.parse(await readFile(resolve(import.meta.dirname, "package.json"), "utf-8"));
-		const tauriConfJson = JSON.parse(await readFile(resolve(import.meta.dirname, "src-tauri", "tauri.conf.json"), "utf-8"));
-		const tauriCargoToml = TOML.parse(await readFile(resolve(import.meta.dirname, "src-tauri", "Cargo.toml"), "utf-8"));
-
-		const version = packageJson.version + "+" + revcount;
-		packageJson.version = version;
-		tauriConfJson.version = version;
-		tauriCargoToml.package.version = version;
-
-		await writeFile(resolve(import.meta.dirname, "package.json"), JSON.stringify(packageJson, undefined, 2), "utf-8");
-		await writeFile(resolve(import.meta.dirname, "src-tauri", "tauri.conf.json"), JSON.stringify(tauriConfJson, undefined, 2), "utf-8");
-		await writeFile(resolve(import.meta.dirname, "src-tauri", "Cargo.toml"), TOML.stringify(tauriCargoToml), "utf-8");
-
-		if(process.env.GITHUB_ENV){
-			await appendFile(process.env.GITHUB_ENV, `AMPERSAND_VERSION=${version}`, "utf-8");
-			console.log("Appended $AMPERSAND_VERSION to GitHub Actions environment.")
-		}
-	}
+	const packageJson = JSON.parse(await readFile(resolve(import.meta.dirname, "package.json"), "utf-8"));
+	return packageJson.version + "+" + revcount;
 }
 
-main();
+//
+// MAIN CODE
+//
+
+const version = await getVersion();
+console.log("New version is", version);
+
+// Read manifest files
+const packageJson = JSON.parse(await readFile(resolve(import.meta.dirname, "package.json"), "utf-8"));
+const tauriConfJson = JSON.parse(await readFile(resolve(import.meta.dirname, "src-tauri", "tauri.conf.json"), "utf-8"));
+const tauriCargoToml = TOML.parse(await readFile(resolve(import.meta.dirname, "src-tauri", "Cargo.toml"), "utf-8"));
+
+// Modify parsed manifests
+packageJson.version = version;
+tauriConfJson.version = version;
+tauriCargoToml.package.version = version;
+
+// Write modified manifests
+await writeFile(resolve(import.meta.dirname, "package.json"), JSON.stringify(packageJson, undefined, 2), "utf-8");
+await writeFile(resolve(import.meta.dirname, "src-tauri", "tauri.conf.json"), JSON.stringify(tauriConfJson, undefined, 2), "utf-8");
+await writeFile(resolve(import.meta.dirname, "src-tauri", "Cargo.toml"), TOML.stringify(tauriCargoToml), "utf-8");
+
+// If in CI, export as env
+if(process.env.GITHUB_ENV){
+	await appendFile(process.env.GITHUB_ENV, `AMPERSAND_VERSION=${version}`, "utf-8");
+	console.log("Appended $AMPERSAND_VERSION to GitHub Actions environment.")
+}
