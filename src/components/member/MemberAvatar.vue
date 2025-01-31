75315e54906f18d4bd898a2f5ed983d059b7e2c4
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-01-31 23:18:24 +0100

    fix default avatar color

--- src/components/member/MemberAvatar.vue
+++ src/components/member/MemberAvatar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 	import {
 		IonAvatar,
+		IonIcon,
 	} from "@ionic/vue";
 
 	import { Member } from '../../lib/db/entities';
@@ -8,15 +9,25 @@
 	import { PartialBy } from "../../lib/types";
 
 	import accountCircle from "@material-symbols/svg-600/outlined/account_circle.svg";
+	import { ref, watch } from "vue";
 
 	const props = defineProps<{
 		member: PartialBy<Member, "uuid">,
 	}>();
+
+	const cssColor = ref("var(--ion-color-primary)");
+
+	watch(props.member, () => {
+		cssColor.value = props.member.color && props.member.color !== "#000000"
+			? props.member.color
+			: "var(--ion-color-primary)";
+	}, { immediate: true });
 </script>
 
 <template>
 	<IonAvatar class="with-outline">
-		<img aria-hidden="true" :src="props.member.image ? getObjectURL(props.member.image) : accountCircle" />
+		<img aria-hidden="true" :src="getObjectURL(props.member.image)" v-if="props.member.image" />
+		<IonIcon :icon="accountCircle" v-else />
 	</IonAvatar>
 </template>
 
@@ -24,6 +35,12 @@
 	ion-avatar.with-outline {
 		outline-width: 2px;
 		outline-style: solid;
-		outline-color: v-bind('props.member.color');
+		outline-color: v-bind('cssColor');
+	}
+
+	ion-icon {
+		width: 100%;
+		height: 100%;
+		color: v-bind('cssColor');
 	}
 </style>
