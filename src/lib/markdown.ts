1fe64c506fba49ec3a5eea12027eb30a4a3969a1
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-02-01 13:57:22 +0100

    add md spoiler tag

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -44,8 +44,34 @@ const mention: TokenizerAndRendererExtension = {
 	}
 };
 
+const spoiler: TokenizerAndRendererExtension = {
+	name: "spoiler",
+	level: "inline",
+	start(src: string) { return src.match(/\|\|/)?.index; },
+	tokenizer(src: string) {
+		const rule = /^\|\|(.+?)\|\|/;
+		const match = rule.exec(src);
+		if (match) {
+			const token = {
+				type: 'spoiler',
+				raw: match[0],
+				text: match[1],
+				tokens: this.lexer.inlineTokens(match[1])
+			};
+			return token;
+		}
+		return;
+	},
+	renderer(token) {
+		return h('span', {
+			tabindex: -1,
+			class: "spoiler"
+		}, token.tokens && token.tokens.length ? this.parser.parseInline(token.tokens) : token.text);
+	}
+};
+
 marked.use({
-	extensions: [mention],
+	extensions: [mention, spoiler],
 	renderer: {
 		image(token) {
 			// checking for lone surrogates the shitty way
