98e2e6168bf110f5a8c7d65b448ac81ba7780b3f
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-02-01 02:39:06 +0100

    wire up import

--- src/views/onboarding/1-Import.vue
+++ src/views/onboarding/1-Import.vue
@@ -2,10 +2,24 @@
     import { IonContent, IonPage, IonButton, useIonRouter } from '@ionic/vue';
 	import { ref } from 'vue';
 	import Spinner from '../../components/Spinner.vue';
+	import { importDatabaseFromBinary } from '../../lib/db/ioutils';
+	import { getFiles } from '../../lib/util/misc';
 
 	const loading = ref(false);
 
 	const router = useIonRouter();
+
+	async function importFromPreviousInstallation() {
+		const files = await getFiles(undefined, false);
+		if (files.length) {
+			loading.value = true;
+
+			const file = files[0];
+			await importDatabaseFromBinary(new Uint8Array(await file.arrayBuffer()));
+		}
+
+		router.replace("/onboarding/end");
+	}
 </script>
 
 <template>
@@ -14,11 +28,11 @@
 			<div class="import-container" v-if="!loading">
 				<h1>{{ $t("onboarding:importScreen.header") }}</h1>
 
-				<IonButton @click="loading = true">
+				<IonButton @click="importFromPreviousInstallation">
 					{{ $t("onboarding:importScreen.prevInstall") }}
 				</IonButton>
 
-				<IonButton @click="router.replace('/onboarding/system/')">
+				<IonButton fill="clear" @click="router.replace('/onboarding/system/')">
 					{{ $t("onboarding:importScreen.startFromScratch") }}
 				</IonButton>
 			</div>
