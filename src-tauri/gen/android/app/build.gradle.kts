52b03aca1fd6373c58f884316f72c70cde41491e
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-02-02 12:53:53 +0100

    update gradle dependencies

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -82,12 +82,12 @@ rust {
 }
 
 dependencies {
-    implementation("androidx.webkit:webkit:1.6.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 apply(from = "tauri.build.gradle.kts")
