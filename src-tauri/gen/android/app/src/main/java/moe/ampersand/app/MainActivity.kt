0c90e88b71d29f32c40e0392231103b3df081ff4
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-02-02 12:02:20 +0100

    use m3 at native side anyway

--- src-tauri/gen/android/app/src/main/java/moe/ampersand/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/moe/ampersand/app/MainActivity.kt
@@ -2,9 +2,16 @@ package moe.ampersand.app
 
 import android.webkit.WebView
 import android.annotation.SuppressLint
+import android.os.Bundle
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : TauriActivity() {
     private lateinit var wv: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        DynamicColors.applyToActivityIfAvailable(this);
+    }
     
     override fun onWebViewCreate(webView: WebView) { 
         wv = webView
@@ -15,4 +22,5 @@ class MainActivity : TauriActivity() {
     override fun onBackPressed() {
         wv.evaluateJavascript("document.dispatchEvent(new Event('backbutton'))".trimIndent()) { _ -> }
     }
+
 }
