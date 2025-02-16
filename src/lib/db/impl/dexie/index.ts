1bbdeaa9342abec2e2bed59c00d5e08c55ff72c2
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-02-16 12:38:37 +0100

    index the right stuff

--- src/lib/db/impl/dexie/index.ts
+++ src/lib/db/impl/dexie/index.ts
@@ -4,15 +4,15 @@ import { AmpersandDexieDatabase } from './types';
 export const db = new Dexie("ampersandDatabase") as AmpersandDexieDatabase;
 
 db.version(1).stores({
-	boardMessages: "uuid, member, title",
+	boardMessages: "uuid, date",
 	chats: "uuid, name",
-	chatMessages: "uuid, chat, member",
-	frontingEntries: "uuid, member, customStatus",
-	journalPosts: "uuid, member, title",
-	members: "uuid, name, isArchived, isCustomFront",
-	reminders: "uuid, name",
-	system: "uuid", // do we even?
-	tags: "uuid, name",
+	chatMessages: "uuid, chat, date",
+	frontingEntries: "uuid, startTime",
+	journalPosts: "uuid, date",
+	members: "uuid",
+	reminders: "uuid",
+	system: "uuid",
+	tags: "uuid",
 	assets: "uuid",
 	customFields: "uuid"
 });
