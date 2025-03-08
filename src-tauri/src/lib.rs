c6fd6464bd02c1a45857ac7c87ab593317472ac5
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-03-08 21:39:53 +0100

    remove some plugins we don't use

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,28 +1,23 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_m3::init())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_persisted_scope::init())
-        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_ampersand::init())
-        .setup(|app: &mut tauri::App| {
+        .setup(|_app: &mut tauri::App| {
             #[cfg(mobile)]
-            app.handle()
+            _app.handle()
                 .plugin(tauri_plugin_biometric::init())
                 .expect("error while running Ampersand");
 
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
+            #[cfg(mobile)]
+            _app.handle()
+                .plugin(tauri_plugin_m3::init())
+                .expect("error while running Ampersand");
             Ok(())
         })
         .run(tauri::generate_context!())
