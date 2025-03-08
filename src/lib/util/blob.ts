ca44e340de921b21b7bb105cbb97f721868f63c0
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-03-09 00:10:36 +0100

    add md5, use it to generate hash for map

--- src/lib/util/blob.ts
+++ src/lib/util/blob.ts
@@ -1,20 +1,27 @@
-const dataURLs = new Map<File, string>();
+import { md5 } from "./md5";
+
+const dataURLs = new Map<string, {
+	file: File,
+	url: string
+}>();
 
 export function getObjectURL(file: File){
-	const maybeEntry = [...dataURLs.entries()].find(([k]) => k.name === file.name);
-
-	if(maybeEntry)
-		return maybeEntry[1];
+	const hash = new TextDecoder().decode(md5(new TextEncoder().encode(file.name + file.lastModified + file.size)));
+	if(dataURLs.has(hash))
+		return dataURLs.get(hash)!.url;
 
 	const url = URL.createObjectURL(file);
 
-	dataURLs.set(file, url);
+	dataURLs.set(hash, {
+		file,
+		url
+	});
 
 	return url;
 }
 
-export async function getFile(url: string): Promise<File | undefined> {
-	return dataURLs.entries().find(([_, _url]) => _url === url)?.[0];
+export function getFile(url: string): File | undefined {
+	return Array.from(dataURLs.values()).find(_obj => _obj.url === url)?.file;
 }
 
 export function getDataURL(url: string): Promise<string | null> {
