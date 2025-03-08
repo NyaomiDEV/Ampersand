41e24aacc10bb5970d76455b84481ba297a76e8f
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-03-08 16:57:17 +0100

    add infinite scroll component

---
+++ src/components/InfiniteScroll.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+	import { onMounted, shallowRef, useTemplateRef } from 'vue';
+	import Spinner from './Spinner.vue';
+
+	const props = defineProps<{
+		callback: () => Promise<void>
+	}>();
+
+	const scroll = useTemplateRef("scroll");
+	const entry = shallowRef<IntersectionObserverEntry>();
+
+	async function callback(entries: IntersectionObserverEntry[]){
+		if(entries[0])
+			entry.value = entries[0];
+
+		await checkAndRun();
+	}
+
+	async function checkAndRun(){
+		if(entry.value?.isIntersecting){
+			await props.callback();
+			setTimeout(checkAndRun, 500);
+		}
+	}
+
+	onMounted(() => {
+		if(!scroll.value) return;
+
+		const observer = new IntersectionObserver(callback, {
+			root: scroll.value?.parentElement,
+			threshold: 0.1
+		});
+
+		observer.observe(scroll.value);
+	});
+</script>
+
+<template>
+	<div class="infinite-scroll" ref="scroll">
+		<Spinner size="24px" />
+	</div>
+</template>
+
+<style scoped>
+	.infinite-scroll {
+		padding: 8px;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
