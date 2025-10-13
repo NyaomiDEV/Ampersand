171b01f5f85e4e943bda3cee123f692a217ccdf3
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-10-13 18:56:22 +0200

    oh god

--- src/lib/markdown/textColorExtension.ts
+++ src/lib/markdown/textColorExtension.ts
@@ -8,7 +8,7 @@ const textColorExtension: MarkedExtension<(VNode | string)[], VNode | string> = 
 			level: "inline",
 			start(src: string) { return src.match(/\[#/)?.index; },
 			tokenizer(src: string) {
-				const rule = /^\[(#[a-fA-F0-9]{3,6}(?:;#[a-fA-F0-9]{3,6})?)\](.+?)\[\/(#[a-fA-F0-9]{3,6}(?:;#[a-fA-F0-9]{3,6})?)?\]/;
+				const rule = /^\[((?:#[a-fA-F0-9]{3,6})?(?:;#[a-fA-F0-9]{3,6})?)\](.+?)\[\/((?:#[a-fA-F0-9]{3,6})?(?:;#[a-fA-F0-9]{3,6})?)?\]/;
 				const match = rule.exec(src);
 				if (match) {
 					const token = {
@@ -27,21 +27,38 @@ const textColorExtension: MarkedExtension<(VNode | string)[], VNode | string> = 
 				const color = (token.color as string).split(";");
 				const colorEnd = (token.colorEnd as string | undefined)?.split(";");
 
-				const text = h("span", {
-					class: "text-color",
-					style: `
-						--markdown-text-color-start: ${color[0]};
-						--markdown-text-color-end: ${colorEnd?.[0] || color[0]};
-					`
-				}, token.tokens && token.tokens.length ? this.parser.parseInline(token.tokens) : token.text);
+				let cssTextStyle = "";
+				if(color[0].length){
+					cssTextStyle += `--markdown-text-color-start: ${color[0]};`;
+					if(colorEnd?.[0].length)
+						cssTextStyle += `--markdown-text-color-end: ${colorEnd[0]};`;
+					else
+						cssTextStyle += `--markdown-text-color-end: ${color[0]};`;
+				}
 
-				if(color[1]){
+				let cssBgStyle = "";
+				if (color[1]?.length) {
+					cssBgStyle += `--markdown-bg-color-start: ${color[1]};`;
+					if (colorEnd?.[1]?.length)
+						cssBgStyle += `--markdown-bg-color-end: ${colorEnd[1]};`;
+					else
+						cssBgStyle += `--markdown-bg-color-end: ${color[1]};`;
+				}
+
+				const text = h(
+					"span",
+					cssTextStyle.length
+						?	{
+								class: "text-color",
+								style: cssTextStyle
+							}
+						: {},
+					token.tokens && token.tokens.length ? this.parser.parseInline(token.tokens) : token.text);
+
+				if(cssBgStyle.length){
 					return h("span", {
 						class: "text-color-bg",
-						style: `
-						--markdown-bg-color-start: ${color[1]};
-						--markdown-bg-color-end: ${colorEnd?.[1] || color[1]};
-					`
+						style: cssBgStyle
 					}, text);
 				}
 				
