428dc7f58d52963659b90c8e9d2b136d675b1e5c
mshaze <ms-haze@riseup.net>
2025-10-16 17:11:04 -0400

    configured rustfmt to use tabs; rustfmt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,30 +2,30 @@ mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_store::Builder::new().build())
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_ampersand::init())
-        .invoke_handler(tauri::generate_handler![
-            commands::our_temp_dir,
-            commands::clear_temp_dir
-        ])
-        .setup(|_app: &mut tauri::App| {
-            #[cfg(mobile)]
-            _app.handle()
-                .plugin(tauri_plugin_biometric::init())
-                .expect("error while running Ampersand");
+	tauri::Builder::default()
+		.plugin(tauri_plugin_fs::init())
+		.plugin(tauri_plugin_http::init())
+		.plugin(tauri_plugin_os::init())
+		.plugin(tauri_plugin_store::Builder::new().build())
+		.plugin(tauri_plugin_opener::init())
+		.plugin(tauri_plugin_dialog::init())
+		.plugin(tauri_plugin_ampersand::init())
+		.invoke_handler(tauri::generate_handler![
+			commands::our_temp_dir,
+			commands::clear_temp_dir
+		])
+		.setup(|_app: &mut tauri::App| {
+			#[cfg(mobile)]
+			_app.handle()
+				.plugin(tauri_plugin_biometric::init())
+				.expect("error while running Ampersand");
 
-            #[cfg(mobile)]
-            _app.handle()
-                .plugin(tauri_plugin_m3::init())
-                .expect("error while running Ampersand");
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running Ampersand");
+			#[cfg(mobile)]
+			_app.handle()
+				.plugin(tauri_plugin_m3::init())
+				.expect("error while running Ampersand");
+			Ok(())
+		})
+		.run(tauri::generate_context!())
+		.expect("error while running Ampersand");
 }
