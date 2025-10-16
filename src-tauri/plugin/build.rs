7dca5c010abad7ab2fba3d49777059c58b1f44ab
mshaze <ms-haze@riseup.net>
2025-10-16 17:11:04 -0400

    configured rustfmt to use tabs; rustfmt

--- src-tauri/plugin/build.rs
+++ src-tauri/plugin/build.rs
@@ -1,17 +1,17 @@
 const COMMANDS: &[&str] = &[
-    "exit_app",
-    "open_file",
-    "test_db",
-    "set_can_go_back",
-    "get_webkit_version",
-    "broadcast_event",
-    "list_assets",
-    "registerListener",
+	"exit_app",
+	"open_file",
+	"test_db",
+	"set_can_go_back",
+	"get_webkit_version",
+	"broadcast_event",
+	"list_assets",
+	"registerListener",
 ];
 
 fn main() {
-    tauri_plugin::Builder::new(COMMANDS)
-        .android_path("android")
-        .ios_path("ios")
-        .build();
+	tauri_plugin::Builder::new(COMMANDS)
+		.android_path("android")
+		.ios_path("ios")
+		.build();
 }
--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -9,27 +9,27 @@ use tauri_plugin_ampersand::Result;
 
 #[command]
 pub fn our_temp_dir() -> Result<String> {
-    let mut temp_path = std::env::temp_dir();
-    temp_path.push("ampersandTemp");
-    fs::create_dir_all(temp_path.as_path())
-        .map_err(Into::into)
-        .and(Ok(temp_path))
-        .map(|x| x.into_os_string().into_string().unwrap())
+	let mut temp_path = std::env::temp_dir();
+	temp_path.push("ampersandTemp");
+	fs::create_dir_all(temp_path.as_path())
+		.map_err(Into::into)
+		.and(Ok(temp_path))
+		.map(|x| x.into_os_string().into_string().unwrap())
 }
 
 #[command]
 pub fn clear_temp_dir() -> Result<()> {
-    let temp_dir = our_temp_dir()?;
-    for entry in fs::read_dir(temp_dir)? {
-        let entry = entry?;
-        let filetype = &entry.file_type()?;
-        if filetype.is_dir() {
-            std::fs::remove_dir_all(entry.path())?;
-        }
-        if filetype.is_file() {
-            std::fs::remove_file(entry.path())?;
-        }
-    }
+	let temp_dir = our_temp_dir()?;
+	for entry in fs::read_dir(temp_dir)? {
+		let entry = entry?;
+		let filetype = &entry.file_type()?;
+		if filetype.is_dir() {
+			std::fs::remove_dir_all(entry.path())?;
+		}
+		if filetype.is_file() {
+			std::fs::remove_file(entry.path())?;
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
