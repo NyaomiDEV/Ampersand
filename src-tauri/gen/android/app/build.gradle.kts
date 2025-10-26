576c8914b90f04cc6cbc8f54edb65df8b49d4b3e
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-10-26 16:31:05 +0100

    i failed yet again

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
             isMinifyEnabled = true
             manifestPlaceholders["appName"] = "@string/app_name"
             base.archivesName.set("ampersand")
-            if(System.getenv("CI") !== null && System.getenv("AMPERSAND_RELEASE") === null) {
+            if(System.getenv("CI") !== null && System.getenv("GITHUB_REF_NAME") === "main") {
                 applicationIdSuffix = ".ci"
                 manifestPlaceholders["appName"] = "@string/app_name_ci"
                 base.archivesName.set("ampersand-ci")
