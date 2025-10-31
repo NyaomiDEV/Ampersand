c5aae02fc925e231fb69a4a327905d8cfd1a4c91
mshaze <ms-haze@riseup.net>
2025-10-31 13:02:38 -0400

    I uhh, oops

--- assets/migrations/02_presence_entries.sql
+++ assets/migrations/02_presence_entries.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS presence_entries (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
 	fronting_entry BLOB CHECK(length(fronting_entry) = 16) NOT NULL REFERENCES fronting_entries,
+	date DATETIME NOT NULL,
 	presence INTEGER CHECK(presence >0 AND presence <=10) NOT NULL
 );
