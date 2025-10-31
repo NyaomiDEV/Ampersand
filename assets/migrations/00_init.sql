737019526e0cf42a1e22b44a711a112b35b96db7
mshaze <ms-haze@riseup.net>
2025-10-31 12:42:37 -0400

    let's just pretend I did this right the first time

--- assets/migrations/00_init.sql
+++ assets/migrations/00_init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS board_messages (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	member BLOB CHECK(length(id) = 16) NOT NULL REFERENCES members,
+	member BLOB CHECK(length(member) = 16) NOT NULL REFERENCES members,
 	title TEXT NOT NULL,
 	body TEXT NOT NULL,
 	date DATETIME NOT NULL,
 	is_pinned BOOLEAN,
 	is_archived BOOLEAN NOT NULL,
-	poll BLOB CHECK(length(id) = 16) REFERENCES polls
+	poll BLOB CHECK(length(poll) = 16) REFERENCES polls
 );
 
 CREATE TABLE IF NOT EXISTS polls (
@@ -16,37 +16,37 @@ CREATE TABLE IF NOT EXISTS polls (
 
 CREATE TABLE IF NOT EXISTS poll_entries (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	poll BLOB CHECK(length(id) = 16) NOT NULL REFERENCES polls,
+	poll BLOB CHECK(length(poll) = 16) NOT NULL REFERENCES polls,
 	choice TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS votes (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	poll_entry BLOB CHECK(length(id) = 16) NOT NULL REFERENCES poll_entries,
-	member BLOB CHECK(length(id) = 16) NOT NULL REFERENCES members,
+	poll_entry BLOB CHECK(length(poll_entry) = 16) NOT NULL REFERENCES poll_entries,
+	member BLOB CHECK(length(member) = 16) NOT NULL REFERENCES members,
 	reason TEXT
 );
 
 CREATE TABLE IF NOT EXISTS fronting_entries (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	member BLOB CHECK(length(id) = 16) NOT NULL REFERENCES members,
+	member BLOB CHECK(length(member) = 16) NOT NULL REFERENCES members,
 	start_time DATETIME NOT NULL,
 	end_time DATETIME,
 	is_main_fronter BOOLEAN NOT NULL,
 	is_locked BOOLEAN NOT NULL,
 	custom_status TEXT,
-	influencing BLOB CHECK(length(id) = 16) REFERENCES members,
+	influencing BLOB CHECK(length(influencing) = 16) REFERENCES members,
 	comment TEXT
 );
 
 CREATE TABLE IF NOT EXISTS journal_posts (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	member BLOB CHECK(length(id) = 16) NOT NULL REFERENCES members,
+	member BLOB CHECK(length(member) = 16) NOT NULL REFERENCES members,
 	date DATETIME NOT NULL,
 	title TEXT NOT NULL,
 	subtitle TEXT,
 	body TEXT NOT NULL,
-	cover BLOB CHECK(length(id) = 16) REFERENCES files,
+	cover BLOB CHECK(length(cover) = 16) REFERENCES files,
 	is_pinned BOOLEAN NOT NULL,
 	is_private BOOLEAN NOT NULL,
 	content_warning TEXT
@@ -54,8 +54,8 @@ CREATE TABLE IF NOT EXISTS journal_posts (
 
 CREATE TABLE IF NOT EXISTS journal_post_tags (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	post BLOB CHECK(length(id) = 16) NOT NULL REFERENCES journal_posts,
-	tag BLOB CHECK(length(id) = 16) NOT NULL REFERENCES tags
+	post BLOB CHECK(length(post) = 16) NOT NULL REFERENCES journal_posts,
+	tag BLOB CHECK(length(tag) = 16) NOT NULL REFERENCES tags
 );
 
 CREATE TABLE IF NOT EXISTS members (
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS members (
 	pronouns TEXT,
 	description TEXT,
 	role TEXT,
-	image BLOB CHECK(length(id) = 16) REFERENCES files,
-	cover BLOB CHECK(length(id) = 16) REFERENCES files,
+	image BLOB CHECK(length(image) = 16) REFERENCES files,
+	cover BLOB CHECK(length(cover) = 16) REFERENCES files,
 	color TEXT,
 	is_pinned BOOLEAN,
 	is_archived BOOLEAN NOT NULL,
@@ -75,22 +75,22 @@ CREATE TABLE IF NOT EXISTS members (
 
 CREATE TABLE IF NOT EXISTS member_tags (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	member BLOB CHECK(length(id) = 16) NOT NULL REFERENCES members,
-	tag BLOB CHECK(length(id) = 16) NOT NULL REFERENCES tags
+	member BLOB CHECK(length(member) = 16) NOT NULL REFERENCES members,
+	tag BLOB CHECK(length(tag) = 16) NOT NULL REFERENCES tags
 );
 
 CREATE TABLE IF NOT EXISTS systems (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
 	name TEXT NOT NULL,
 	description TEXT,
-	image BLOB CHECK(length(id) = 16) REFERENCES files
+	image BLOB CHECK(length(image) = 16) REFERENCES files
 );
 
 CREATE TABLE IF NOT EXISTS tags (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
 	name TEXT NOT NULL,
 	description TEXT,
-	type BLOB CHECK(length(id) = 16) NOT NULL REFERENCES tag_types,
+	type BLOB CHECK(length(type) = 16) NOT NULL REFERENCES tag_types,
 	color TEXT,
 	view_in_lists BOOLEAN NOT NULL
 );
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS tag_types (
 
 CREATE TABLE IF NOT EXISTS assets (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	file BLOB CHECK(length(id) = 16) NOT NULL REFERENCES files,
+	file BLOB CHECK(length(file) = 16) NOT NULL REFERENCES files,
 	friendly_name TEXT NOT NULL
 );
 
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS custom_fields (
 
 CREATE TABLE IF NOT EXISTS custom_field_data (
 	id BLOB CHECK(length(id) = 16) PRIMARY KEY,
-	field BLOB CHECK(length(id) = 16) NOT NULL REFERENCES custom_fields,
+	field BLOB CHECK(length(field) = 16) NOT NULL REFERENCES custom_fields,
 	value TEXT NOT NULL
 );
 
