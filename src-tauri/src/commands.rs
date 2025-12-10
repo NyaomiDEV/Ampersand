f2303290764674f576a0aaebf07920926dbc56ca
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-12-10 16:04:20 +0100

    rust: remove old cache related commands

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,37 +1,9 @@
-use std::fs;
-
 use tauri::command;
 use tauri_plugin_ampersand::Result;
 
 // Platform independent commands and code goes here. Here we shill pure Rust or almost that.
 // Commands that have to ALSO or EXCLUSIVELY interface with NATIVE INTERFACES (Android/iOS)
 // go into src-tauri/plugin/src/{commands.rs,mobile.rs,desktop.rs}
-
-#[command]
-pub fn our_temp_dir() -> Result<String> {
-	let mut temp_path = std::env::temp_dir();
-	temp_path.push("ampersandTemp");
-	Ok(fs::create_dir_all(temp_path.as_path())
-		.and(Ok(temp_path))
-		.map(|x| x.into_os_string().into_string().unwrap())?)
-}
-
-#[command]
-pub fn clear_temp_dir() -> Result<()> {
-	let temp_dir = our_temp_dir()?;
-	for entry in fs::read_dir(temp_dir)? {
-		let entry = entry?;
-		let filetype = &entry.file_type()?;
-		if filetype.is_dir() {
-			std::fs::remove_dir_all(entry.path())?;
-		}
-		if filetype.is_file() {
-			std::fs::remove_file(entry.path())?;
-		}
-	}
-
-	Ok(())
-}
 
 #[command]
 pub fn get_webkit_version() -> Result<String> {
