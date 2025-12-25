e680212b9e0688872f909d1f4fd8064c373e2ce5
Naomi Calabretta <nyaomicalabretta@gmail.com>
2025-12-25 11:58:44 +0100

    create html5 web history in itself

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "@ionic/vue-router";
+import { createRouter, createWebHistory } from "@ionic/vue-router";
 import { RouteRecordRaw } from "vue-router";
 import tabbedRoutes from "./tabbedRoutes";
 import options from "./options";
@@ -27,7 +27,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
 });
 
