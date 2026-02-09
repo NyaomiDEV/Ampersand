521ef47530a21b9ac77899d7eab25f62085a4653
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-02-09 15:03:36 +0100

    cargo fmt on plugin

--- src-tauri/plugin/build.rs
+++ src-tauri/plugin/build.rs
@@ -1,8 +1,18 @@
-const COMMANDS: &[&str] = &["open_file", "get_webkit_version", "broadcast_event", "list_assets", "dismiss_splash", "register_listener", "test_db", "run_db_migrations", "migrate_old_db"];
+const COMMANDS: &[&str] = &[
+	"open_file",
+	"get_webkit_version",
+	"broadcast_event",
+	"list_assets",
+	"dismiss_splash",
+	"register_listener",
+	"test_db",
+	"run_db_migrations",
+	"migrate_old_db",
+];
 
 fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
+	tauri_plugin::Builder::new(COMMANDS)
+		.android_path("android")
+		.ios_path("ios")
+		.build();
 }
--- src-tauri/plugin/src/lib.rs
+++ src-tauri/plugin/src/lib.rs
@@ -1,8 +1,8 @@
 use rusqlite::Connection;
 use std::sync::Mutex;
 use tauri::{
-  plugin::{Builder, TauriPlugin},
-  Manager, Runtime,
+	plugin::{Builder, TauriPlugin},
+	Manager, Runtime,
 };
 
 #[cfg(desktop)]
@@ -22,38 +22,38 @@ use mobile::Ampersand;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the ampersand APIs.
 pub trait AmpersandExt<R: Runtime> {
-  fn ampersand(&self) -> &Ampersand<R>;
+	fn ampersand(&self) -> &Ampersand<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> crate::AmpersandExt<R> for T {
-  fn ampersand(&self) -> &Ampersand<R> {
-    self.state::<Ampersand<R>>().inner()
-  }
+	fn ampersand(&self) -> &Ampersand<R> {
+		self.state::<Ampersand<R>>().inner()
+	}
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-    Builder::new("ampersand")
-  .invoke_handler(tauri::generate_handler![
-    commands::open_file,
-    commands::broadcast_event,
-    commands::list_assets,
-    commands::dismiss_splash,
-    commands::test_db,
-    commands::run_db_migrations,
-    commands::migrate_old_db,
-  ])
-  .setup(|app, api: tauri::plugin::PluginApi<R, ()>| {
-    	std::fs::create_dir_all(app.path().app_data_dir()?)?;
+	Builder::new("ampersand")
+		.invoke_handler(tauri::generate_handler![
+			commands::open_file,
+			commands::broadcast_event,
+			commands::list_assets,
+			commands::dismiss_splash,
+			commands::test_db,
+			commands::run_db_migrations,
+			commands::migrate_old_db,
+		])
+		.setup(|app, api: tauri::plugin::PluginApi<R, ()>| {
+			std::fs::create_dir_all(app.path().app_data_dir()?)?;
 			let db_path = app.path().app_data_dir()?.join("db.sqlite");
-      let db = Connection::open(db_path)?;
+			let db = Connection::open(db_path)?;
 
-      #[cfg(mobile)]
-      let ampersand = mobile::init(app, Mutex::new(db), api)?;
-      #[cfg(desktop)]
-      let ampersand = desktop::init(app, Mutex::new(db), api)?;
-      app.manage(ampersand);
-      Ok(())
-  })
-  .build()
+			#[cfg(mobile)]
+			let ampersand = mobile::init(app, Mutex::new(db), api)?;
+			#[cfg(desktop)]
+			let ampersand = desktop::init(app, Mutex::new(db), api)?;
+			app.manage(ampersand);
+			Ok(())
+		})
+		.build()
 }
