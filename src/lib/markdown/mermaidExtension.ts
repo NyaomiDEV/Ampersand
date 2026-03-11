79868e0eb291e20bb0ba602c4711ea51ca4923cf
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-03-11 15:17:26 +0100

    fix: mermaid why? this is hell

--- src/lib/markdown/mermaidExtension.ts
+++ src/lib/markdown/mermaidExtension.ts
@@ -3,6 +3,150 @@ import { MarkedExtension } from "marked";
 import mermaid from "mermaid";
 import { isDarkMode } from "../mode";
 
+const themeCSS = `
+.error-icon {
+	fill: rgb(var(--md3-on-error));
+}
+
+.error-text {
+	fill: rgb(var(--md3-on-error));
+	stroke: rgb(var(--md3-on-error));
+}
+
+.marker {
+	fill: rgb(var(--md3-on-surface));
+	stroke: rgb(var(--md3-on-surface));
+
+	.cross, &.aggregation, &.extension, &.composition {
+		stroke: rgb(var(--md3-on-surface)) !important;
+	}
+
+	&.composition {
+		fill: rgb(var(--md3-on-surface)) !important;
+	}
+}
+
+span {
+	color: rgb(var(--md3-on-surface));
+}
+
+.chart-title text, .left-axis .title text {
+	fill: rgb(var(--md3-on-surface));
+}
+
+.label {
+	color: rgb(var(--md3-on-surface));
+
+	text {
+		fill: rgb(var(--md3-on-surface));
+		color: rgb(var(--md3-on-surface));
+	}
+}
+
+.cluster-label {
+	text {
+		fill: rgb(var(--md3-on-surface));
+	}
+
+	span {
+		color: rgb(var(--md3-on-surface));
+	}
+}
+
+.node :is(rect, circle, ellipse, polygon, path) {
+	fill: rgb(var(--md3-surface-container-highest));
+	stroke: rgb(var(--md3-on-surface-variant));
+}
+
+.node .label-container path {
+	fill: rgb(var(--md3-surface-container-highest)) !important;
+	stroke: rgb(var(--md3-on-surface-variant)) !important;
+}
+
+.node .katex path {
+  fill: rgb(var(--md3-on-surface));
+  stroke: rgb(var(--md3-on-surface));
+}
+
+.root .anchor path {
+  fill: rgb(var(--md3-on-surface)) !important;
+  stroke: rgb(var(--md3-on-surface));
+}
+
+.arrowheadPath {
+  fill: rgb(var(--md3-on-surface));
+}
+
+.edgePath .path, .edgePaths path {
+  stroke: rgb(var(--md3-on-surface));
+}
+
+.flowchart-link {
+  stroke: rgb(var(--md3-on-surface));
+}
+
+.edgeLabel {
+  background-color: rgb(var(--md3-surface-container));
+
+  p, rect {
+	background-color: rgb(var(--md3-surface-container));
+  }
+}
+
+.labelBkg {
+  background-color: rgba(var(--md3-surface-container), 0.5);
+}
+
+.cluster {
+	rect {
+		fill: rgb(var(--md3-surface-container)) !important;
+		stroke: rgb(var(--md3-on-surface)) !important;
+	}
+
+	text {
+		fill: rgb(var(--md3-on-surface));
+	}
+
+	span {
+		color: rgb(var(--md3-on-surface));
+	}
+}
+
+.flowchartTitleText {
+	fill: rgb(var(--md3-on-surface));
+}
+
+.icon-shape, .image-shape {
+	background-color: rgb(var(--md3-surface-container));
+
+	p, rect {
+		background-color: rgb(var(--md3-surface-container));
+	}
+
+	rect {
+		fill: rgb(var(--md3-surface-container));
+	}
+}
+
+.background {
+	fill: transparent;
+}
+
+.plot {
+	rect {
+		fill: rgb(var(--md3-surface-container-highest));
+	}
+
+	path {
+		stroke: rgb(var(--md3-primary));
+	}
+}
+
+.axis-line path, .axisl-line path, .ticks path {
+	stroke: rgb(var(--md3-on-surface));
+}
+`;
+
 const mermaidExtension: MarkedExtension<(VNode | string)[], VNode | string> = {
 	renderer: {
 		code(token) {
@@ -18,14 +162,21 @@ const mermaidExtension: MarkedExtension<(VNode | string)[], VNode | string> = {
 		switch(token.type){
 			case "code":
 				if (token.lang === "mermaid") {
-					mermaid.initialize({
-						startOnLoad: false,
-						theme: isDarkMode() ? "dark" : "neutral",
-						darkMode: isDarkMode()
-					});
 					const hash = new Uint8Array(await window.crypto.subtle.digest("SHA-256", new TextEncoder().encode(token.text)));
 					// @ts-expect-error this is due to number to string conversion on map(); TODO: substitute with toHex()
 					const hashHex = hash.map((b) => b.toString(16).padStart(2, "0")).join("");
+
+					mermaid.initialize({
+						startOnLoad: false,
+						theme: "base",
+						darkMode: isDarkMode(),
+						fontFamily: "var(--ion-font-family)",
+						securityLevel: "strict",
+						deterministicIds: true,
+						deterministicIDSeed: hashHex,
+						themeCSS
+					});
+
 					const result = await mermaid.render(`mermaid-render-${hashHex}`, token.text);
 					// eslint-disable-next-line @typescript-eslint/no-explicit-any
 					(token as any).renderResult = result.svg;
