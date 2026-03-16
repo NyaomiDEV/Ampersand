4a67dd8c5ce8bb3dbcf844a1b9e6892a97af7a62
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-03-16 10:00:48 +0100

    chore: just... don't even attempt hashing

--- src/lib/util/blob.ts
+++ src/lib/util/blob.ts
@@ -1,25 +1,11 @@
-import { md5 } from "./md5";
-
-const dataURLs = new Map<string, {
-	file: File,
-	url: string
-}>();
+const dataURLs = new Map<string, File>();
 
 export function getObjectURL(file: File){
-	const hash = Array.from(md5(new TextEncoder().encode(file.name + file.lastModified + file.size))).map(x => x.toString(16)).join("");
-	if(dataURLs.has(hash))
-		return dataURLs.get(hash)!.url;
-
 	const url = URL.createObjectURL(file);
-
-	dataURLs.set(hash, {
-		file,
-		url
-	});
-
+	dataURLs.set(url, file);
 	return url;
 }
 
 export function getFile(url: string): File | undefined {
-	return Array.from(dataURLs.values()).find(_obj => _obj.url === url)?.file;
+	return Array.from(dataURLs.entries()).find(_obj => _obj[0] === url)?.[1];
 }
