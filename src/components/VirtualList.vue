9b489a5e855e3b6c1d8b26717778ea02a1c21b8d
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-03-24 21:35:47 +0100

    fix: try to fix the virtual scroller

--- src/components/VirtualList.vue
+++ src/components/VirtualList.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts" generic="T extends UUIDable">
 	import { useVirtualizer, Virtualizer } from "@tanstack/vue-virtual";
-	import { computed, ref } from "vue";
+	import { ComponentPublicInstance, computed, ref } from "vue";
 	import { UUIDable } from "../lib/db/entities";
 
 	const { entries } = defineProps<{
 		entries: T[]
 	}>();
 
-	const scroller = ref<Element>();
+	const scroller = ref<Element | null>(null);
 
 	const virtualizerOptions = computed(() => ({
 		count: entries?.length ?? 0,
-		getScrollElement: () => scroller.value || null,
+		getScrollElement: () => scroller.value,
 		estimateSize: () => 80,
 		getItemKey: (index: number) => entries[index]?.uuid ?? index,
 		measureElement: (element: Element, _entry: ResizeObserverEntry | undefined, instance: Virtualizer<Element, Element>): number => {
@@ -32,35 +32,38 @@
 	const rowVirtualizer = useVirtualizer(virtualizerOptions);
 	const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems());
 	const totalSize = computed(() => rowVirtualizer.value.getTotalSize());
+
+	async function getRootScroller(e: Element | ComponentPublicInstance | null){
+		scroller.value = await (e as Element)?.closest("ion-content")?.getScrollElement() || null;
+	}
 </script>
 
 <template>
-	<div ref="scroller" class="virtualList">
+	<div
+		:ref="getRootScroller"
+		:style="{
+			height: `${totalSize}px`,
+			position: 'relative',
+			width: '100%',
+		}"
+	>
 		<div
+			class="v-container"
 			:style="{
-				height: `${totalSize}px`,
-				position: 'relative',
+				position: 'absolute',
+				top: 0,
+				left: 0,
 				width: '100%',
+				transform: `translateY(${virtualRows[0]?.start ?? 0}px)`,
 			}"
 		>
 			<div
-				class="v-container"
-				:style="{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					transform: `translateY(${virtualRows[0]?.start ?? 0}px)`,
-				}"
+				v-for="vrow in virtualRows"
+				:key="vrow.key.toString()"
+				class="v-row"
+				:data-index="vrow.index"
 			>
-				<div
-					v-for="vrow in virtualRows"
-					:key="vrow.key.toString()"
-					class="v-row"
-					:data-index="vrow.index"
-				>
-					<slot :index="vrow.index" :entry="entries[vrow.index]" />
-				</div>
+				<slot :index="vrow.index" :entry="entries[vrow.index]" />
 			</div>
 		</div>
 	</div>
