631dca108c1edd95074b623e13051616837ff3a0
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-03-30 23:32:39 +0200

    chore: minsdk 29

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "moe.ampersand.app"
-        minSdk = 26
+        minSdk = 29
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
