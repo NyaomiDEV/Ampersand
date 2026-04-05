b2c354d28a0a72b89990aa71440114a166ddd9bb
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-04-05 20:24:22 +0200

    feat: collapsible callouts

--- src/lib/markdown/calloutExtension.ts
+++ src/lib/markdown/calloutExtension.ts
@@ -8,13 +8,14 @@ const calloutExtension: MarkedExtension<(VNode | string)[], VNode | string> = {
 			level: "block",
 			start(src: string) { return src.match(/:::/)?.index; },
 			tokenizer(src: string) {
-				const rule = /^:::(?: \{(?<color>.+?)\})?(?: (?<title>.+?))?\n(?<text>[\S\s]+?)\n:::(?:\n|$)/;
+				const rule = /^:::(?<closed>-)?(?: \{(?<color>.+?)\})?(?: (?<title>.+?))?\n(?<text>[\S\s]+?)\n:::(?:\n|$)/;
 				const match = rule.exec(src);
 				if (match) {
 					const token = {
 						type: "callout",
 						raw: match[0],
 						color: match.groups?.color,
+						closed: match.groups?.closed === "-",
 						text: match.groups?.title ? `${match.groups.title}\n${match.groups?.text}` : match.groups?.text,
 						tokens: this.lexer.inlineTokens(match.groups!.text)
 					};
@@ -30,8 +31,13 @@ const calloutExtension: MarkedExtension<(VNode | string)[], VNode | string> = {
 				return;
 			},
 			renderer(token) {				
-				return h("div", { class: "callout", style: token.color ? `--markdown-callout-color: ${token.color}` : "" },
-					token.tokens && token.tokens.length ? this.parser.parseInline(token.tokens) : token.text
+				return h(token.tokens?.find(t => t.type === "callout-title") ? "details" : "div", {
+					class: "callout",
+					open: !token.closed,
+					style: token.color ? `--markdown-callout-color: ${token.color}` : "",
+					onClick: (e) => e.stopImmediatePropagation()
+				},
+				token.tokens && token.tokens.length ? this.parser.parseInline(token.tokens) : token.text
 				);
 			}
 		},
@@ -39,7 +45,7 @@ const calloutExtension: MarkedExtension<(VNode | string)[], VNode | string> = {
 			name: "callout-title",
 			level: "inline",
 			renderer(token) {
-				return h("span", { class: "callout-title" },
+				return h("summary", { class: "callout-title" },
 					token.tokens && token.tokens.length
 						? this.parser.parseInline(token.tokens)
 						: token.raw
