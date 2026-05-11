bc0c5423ea4e1631daaaafd5988429fe8d66bcce
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-05-11 21:18:11 +0200

    chore: stack in console log

--- src/components/ConsoleLog.vue
+++ src/components/ConsoleLog.vue
@@ -37,6 +37,11 @@
 					{{ obj }}
 				</span>
 			</div>
+			<div v-if="entry.stack" class="stack">
+				<span v-for="parsedStack in entry.stack" :key="parsedStack.code + parsedStack.location + parsedStack.row + parsedStack.col">
+					{{ parsedStack.code }} (at {{ parsedStack.location }}:{{ parsedStack.row }}:{{ parsedStack.col }})
+				</span>
+			</div>
 		</div>
 	</div>
 </template>
@@ -55,32 +60,48 @@
 			padding: 8px 16px;
 			margin: 0;
 			display: grid;
-			grid-template-columns: 1fr 3fr;
-			gap: 2px;
+			grid-template-columns: 0.5fr 1fr 1fr;
+			gap: 8px;
 			border-radius: 4px;
 			background-color: rgb(var(--md3-surface-container));
+			color: rgb(var(--md3-on-surface-container));
+
+			> div {
+				display: block;
+				width: 100%;
+			}
+
+			> div.stack > span {
+				display: block;
+				margin-bottom: .5em;
+
+				&::before {
+					content: "- ";
+				}
+			}
 
 			&.info {
-				color: rgb(var(--md3-primary));
+				color: rgb(var(--md3-on-primary-container));
 				background-color: rgb(var(--md3-primary-container));
 			}
 
 			&.debug {
-				color: rgb(var(--md3-secondary));
+				color: rgb(var(--md3-on-secondary-container));
 				background-color: rgb(var(--md3-secondary-container));
 			}
 
 			&.warn {
-				color: rgb(var(--md3-tertiary));
+				color: rgb(var(--md3-on-tertiary-container));
 				background-color: rgb(var(--md3-tertiary-container));
 			}
 
 			&.error {
-				color: rgb(var(--md3-error));
+				color: rgb(var(--md3-on-error-container));
 				background-color: rgb(var(--md3-error-container));
 			}
 
-			span:has(~ span), span ~ span {
+			> div:not(.stack) > span:has(~ span),
+			> div:not(.stack) > span ~ span {
 				display: inline-block;
 				padding: 2px;
 				border-radius: 2px;
--- src/lib/console.ts
+++ src/lib/console.ts
@@ -5,8 +5,19 @@ type Severity = "log" | "info" | "warn" | "error" | "debug";
 type ConsoleObject = {
 	date: Date,
 	severity: Severity,
+	stack?: ParsedStack[],
 	data: any[]
 };
+
+type ParsedStack = {
+	code: string,
+	location: string,
+	row: number,
+	col: number
+};
+
+const v8Stack = /^at (.*?) \((.*?):([0-9]*?):([0-9]*?)\)$/;
+const jscStack = /^(.*?)@(.*?):([0-9]*?):([0-9]*?)$/;
 
 const oldConsole = {
 	log: window.console.log,
@@ -18,46 +29,87 @@ const oldConsole = {
 
 const store = reactive<ConsoleObject[]>([]);
 
+function normalizeStackTrace(stack?: string){
+	if(!stack) return;
+
+	const stackLines = stack.split("\n").map(x => x.trim());
+	const parsedStack: ParsedStack[] = [];
+	
+	for(const line of stackLines){
+		const matches = v8Stack.exec(line);
+		if(matches !== null){
+			parsedStack.push({
+				code: matches[1],
+				location: matches[2].startsWith("http") ? matches[2].split("/").pop()! : matches[2],
+				row: parseInt(matches[3]),
+				col: parseInt(matches[4])
+			});
+		} else {
+			const matches = jscStack.exec(line);
+			if (matches !== null) {
+				parsedStack.push({
+					code: matches[1],
+					location: matches[2].startsWith("http") ? matches[2].split("/").pop()! : matches[2],
+					row: parseInt(matches[3]),
+					col: parseInt(matches[4])
+				});
+			}
+		}
+	}
+	
+	return parsedStack;
+}
+
 function log(...data){
+	const stack = new Error().stack;
 	store.push({
 		date: new Date(),
 		severity: "log",
+		stack: normalizeStackTrace(stack),
 		data
 	});
 	oldConsole.log(...data);
 }
 
 function info(...data) {
+	const stack = new Error().stack;
 	store.push({
 		date: new Date(),
 		severity: "info",
+		stack: normalizeStackTrace(stack),
 		data
 	});
 	oldConsole.info(...data);
 }
 
 function warn(...data) {
+	const stack = new Error().stack;
 	store.push({
 		date: new Date(),
 		severity: "warn",
+		stack: normalizeStackTrace(stack),
 		data
 	});
 	oldConsole.warn(...data);
 }
 
 function error(...data) {
+	const stack = new Error().stack;
 	store.push({
 		date: new Date(),
 		severity: "error",
+		stack: normalizeStackTrace(stack),
 		data
 	});
 	oldConsole.error(...data);
 }
 
 function debug(...data) {
+	const stack = new Error().stack;
 	store.push({
 		date: new Date(),
 		severity: "debug",
+		stack: normalizeStackTrace(stack),
 		data
 	});
 	oldConsole.debug(...data);
