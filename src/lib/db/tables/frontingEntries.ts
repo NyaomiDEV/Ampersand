f4d82ecc50f9c90e770a20a43d89d4a42cd7331f
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-05-11 18:22:00 +0200

    chore: expose fronting index

--- src/lib/db/tables/frontingEntries.ts
+++ src/lib/db/tables/frontingEntries.ts
@@ -39,6 +39,10 @@ export function getFrontingEntry(uuid: UUID){
 	return db.frontingEntries.get(uuid);
 }
 
+export function getFrontingEntryIndex(){
+	return db.frontingEntries.index;
+}
+
 export async function toFrontingEntryComplete(frontingEntries: FrontingEntry[]): Promise<FrontingEntryComplete[]> {
 	const _memberSet = await Promise.all(Array.from(new Set(
 		frontingEntries.map(x => [x.member, x.influencing].filter((x): x is string => !!x)).flat(1)
