fb26ee9aa9a273b846bc7d4f3479ad3e9f725dda
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-05-14 08:55:24 +0200

    fix: collapsible headerbar

--- src/components/CollapsibleHeaderbar.vue
+++ src/components/CollapsibleHeaderbar.vue
@@ -19,25 +19,35 @@
 		const headerEl = headerbar.value.$el as HTMLElement;
 		const scrollerEl = props.scroller ?? headerEl.parentElement as HTMLElement;
 		const firstToolbarEl = headerEl.getElementsByTagName("ion-toolbar").item(0);
+		if(!firstToolbarEl) return;
 
-		const shadow = await new Promise<ShadowRoot>((res) => {
+		const rtl = window.getComputedStyle(firstToolbarEl, null).getPropertyValue("direction") === "rtl";
+
+		const observer = new MutationObserver((mutations) => {
+			console.log(mutations);
+		});
+
+		observer.observe(firstToolbarEl, { childList: true, subtree: true });
+
+		const shadow = await new Promise<ShadowRoot>((res, rej) => {
+			const timeoutId = setTimeout(() => rej(new Error("timed out")), 5000);
 			const cb = () => {
-				if(firstToolbarEl?.shadowRoot && firstToolbarEl.shadowRoot.children.length)
+				if(firstToolbarEl?.shadowRoot && firstToolbarEl.shadowRoot.children.length){
+					clearTimeout(timeoutId);
 					res(firstToolbarEl.shadowRoot);
-				else
+				} else
 					setTimeout(cb, 1);
 			};
 			cb();
 		});
 
-		contentOffset.value = Math.max(
-			0,
-			(shadow.querySelector<HTMLElement>("[part=content]")?.offsetLeft ?? 0) - 
-				parseFloat(
-					shadow.querySelector<HTMLElement>("[part=container]")?.computedStyleMap()
-						.get("padding-left")?.toString().replace("px", "") || "0"
-				)
-		);
+		const content = shadow.querySelector<HTMLElement>("[part=content]");
+		const container = shadow.querySelector<HTMLElement>("[part=container]");
+		if(!content || !container) return;
+
+		const paddingInline = parseFloat(container.computedStyleMap().get("padding-inline-start")?.toString().replace("px", "") || "0");
+
+		contentOffset.value = Math.max(0, rtl ? ((container.offsetWidth - content.offsetWidth - content.offsetLeft) - paddingInline) : (content.offsetLeft - paddingInline));
 
 		if(props.isCssAnim){
 			if(scrollerEl.tagName.toLowerCase() === "ion-content") {
@@ -67,6 +77,10 @@
 			collapsible: true,
 			'css-anim': props.isCssAnim,
 			'js-anim': !props.isCssAnim
+		}"
+		:style="{
+			'--content-offset': contentOffset,
+			'--scroll-delta': scrollDelta
 		}"
 	>
 		<slot />
@@ -106,13 +120,16 @@
 		}
 
 		&.js-anim {
-			--scroll-delta-normalized: clamp(0, calc(v-bind('scrollDelta') / (var(--transition-size) / 1px)), 1);
+			--scroll-delta-normalized: clamp(0, calc(var(--scroll-delta) / (var(--transition-size) / 1px)), 1);
 			--scroll-delta-remapped: calc(-1 + (var(--scroll-delta-normalized) * 2));
 			transform: translateY(calc(-1 * var(--scroll-delta-normalized) * var(--transition-size)));
 
 			:deep(ion-toolbar:first-child) {
 				&::part(content) {
-					transform: translateX(calc(-1px * v-bind('contentOffset') * (1 - var(--scroll-delta-normalized))));
+					transform: translateX(calc(-1px * var(--content-offset) * (1 - var(--scroll-delta-normalized))));
+					&:dir(rtl){
+						transform: translateX(calc(var(--content-offset) * (1 - var(--scroll-delta-normalized))));
+					}
 				}
 
 				ion-title {
@@ -146,23 +163,23 @@
 			transform: translateY(0);
 
 			:deep(ion-toolbar:first-child) {
-				&::part(content) {
+				:not([slot]) {
 					animation-range-start: 0px;
 					animation-range-end: var(--transition-size);
 					animation-timeline: --scroller;
 					animation-fill-mode: forwards;
-					animation-name: content_anim;
+					animation-name: content_anim_ltr;
 					animation-timing-function: linear;
 
-					transform: translateX(calc(-1px * v-bind('contentOffset')));
+					transform: translateX(calc(-1px * var(--content-offset)));
 				}
 
-				ion-title {
+				ion-title:not([slot]) {
 					animation-range-start: 0px;
 					animation-range-end: var(--transition-size);
 					animation-timeline: --scroller;
 					animation-fill-mode: forwards;
-					animation-name: title_anim;
+					animation-name: content_anim_ltr, title_anim;
 					animation-timing-function: linear;
 
 					font-size: 2rem;
@@ -183,6 +200,18 @@
 					transform: translateY(calc(var(--transition-size) / 2 * -1));
 				}
 			}
+
+			&:dir(rtl) :deep(ion-toolbar:first-child) {
+				:not([slot]) {
+					animation-name: content_anim_rtl;
+					transform: translateX(calc(1px * var(--content-offset)));
+				}
+				
+				ion-title:not([slot]) {
+					animation-name: content_anim_rtl, title_anim;
+					transform: translateX(calc(1px * var(--content-offset)));
+				}
+			}
 		}
 	}
 
@@ -195,9 +224,18 @@
 		}
 	}
 
-	@keyframes content_anim {
+	@keyframes content_anim_ltr {
 		0% {
-			transform: translateX(calc(-1px * v-bind('contentOffset')));
+			transform: translateX(calc(-1px * var(--content-offset)));
+		}
+		100% {
+			transform: translateX(0);
+		}
+	}
+
+	@keyframes content_anim_rtl {
+		0% {
+			transform: translateX(calc(1px * var(--content-offset)));
 		}
 		100% {
 			transform: translateX(0);
