3fe1e7bf5df092979d8f0f0899bc80a2a2feed8e
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-06-07 10:14:34 +0200

    chore: guess who forgot to commit some stuff

--- build_playstore.sh
+++ build_playstore.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export AMPERSAND_BUILD_TYPE="stable-oldpackage"
-export AMPERSAND_GOES_TO_PLAY_STORE=1
+export AMPERSAND_BUILD_TYPE=rc
+export AMPERSAND_VERSION=0.3.0-rc2
+export AMPERSAND_GOES_TO_APP_STORE=1
 
 node upgrade-vcs.mjs
 yarn tauri android build --aab --target aarch64 armv7
