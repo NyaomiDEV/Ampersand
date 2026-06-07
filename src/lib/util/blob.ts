947cddee9c32f004943f7588882f68e4993e331e
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-06-07 20:17:39 +0200

    fix: blob optimizations

--- src/lib/util/blob.ts
+++ src/lib/util/blob.ts
@@ -1,40 +1,55 @@
 import { onUnmounted } from "vue";
 import { getExtension } from "../mime";
 
+const globalDataURLs = new Map<string, [File, number]>();
+
 export function useBlob(){
-	const dataURLs = new Map<string, WeakRef<File>>();
+	const usedDataURLs: Set<string> = new Set();
 
 	onUnmounted(clear);
 
 	function getObjectURL(file: File) {
-		const maybeCachedEntry = dataURLs.entries().find(x => x[1].deref() === file);
-		if (maybeCachedEntry) return maybeCachedEntry[0];
-
+		for(const [url, fileref] of globalDataURLs){
+			const _file = fileref[0];
+			if(_file === file){
+				if(!usedDataURLs.has(url)){
+					fileref[1]++;
+					usedDataURLs.add(url);
+				}
+				return url;
+			}
+		}
 		const url = URL.createObjectURL(file);
-		dataURLs.set(url, new WeakRef(file));
+		globalDataURLs.set(url, [file, 1]);
+		usedDataURLs.add(url);
 		return url;
 	}
 
 	function revokeObjectURL(urlOrFile: string | File){
-		const url = dataURLs.entries().find(x => {
+		const url = globalDataURLs.entries().find(x => {
 			if(urlOrFile instanceof File)
-				return x[1].deref() === urlOrFile;
+				return x[1][0] === urlOrFile;
 			return x[0] === urlOrFile;
 		})?.[0];
 
 		if(url){
-			URL.revokeObjectURL(url);
-			dataURLs.delete(url);
+			const _scope = globalDataURLs.get(url);
+			if(!_scope) return;
+			_scope[1]--;
+			if(_scope[1] < 1){
+				globalDataURLs.delete(url);
+				URL.revokeObjectURL(url);
+			}
+			usedDataURLs.delete(url);
 		}
 	}
 
 	function getFile(url: string): File | undefined {
-		return Array.from(dataURLs.entries()).find(_obj => _obj[0] === url)?.[1].deref();
+		return Array.from(globalDataURLs).find(_obj => _obj[0] === url)?.[1][0];
 	}
 
 	function clear(){
-		dataURLs.entries().forEach(x => URL.revokeObjectURL(x[0]));
-		dataURLs.clear();
+		usedDataURLs.forEach(x => revokeObjectURL(x));
 	}
 
 	return {
