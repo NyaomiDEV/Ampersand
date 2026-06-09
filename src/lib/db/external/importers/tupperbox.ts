655788c28861c41b82674e717e8f735e259857b5
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-06-09 08:19:24 +0200

    chore: make subsystems for tupper

--- src/lib/db/external/importers/tupperbox.ts
+++ src/lib/db/external/importers/tupperbox.ts
@@ -1,7 +1,6 @@
 import { appConfig } from "../../../config";
 import { clearAllDatabase } from "../..";
 import { newSystem } from "../../tables/system";
-import { newTag } from "../../tables/tags";
 import { newMember, updateMember } from "../../tables/members";
 import { parseJsonStreamWithPaths, streamToIterable } from "json-stream-es";
 import { transactionSucceeded } from "../../utils";
@@ -10,31 +9,33 @@ import { getImage } from "../utils";
 import { open } from "../../../native/open";
 import { intoStream } from "../../../native/fs";
 
-//function deriveHash(discordId: string, imageString: string){
-//	return `https://cdn.tupperbox.app/${discordId}/${imageString}.webp`;
-//}
+function deriveHash(imageString: string | null, discordId?: string){
+	if(!discordId || !imageString) return;
+	return `https://cdn.tupperbox.app/${discordId}/${imageString}.webp`;
+}
 
-async function tag(tuGroup: TupperboxGroup, tagMapping: Map<number, string>){
-	const result = await newTag({
+async function subsystems(tuGroup: TupperboxGroup, systemMapping: Map<number, string>, discordId?: string){
+	const result = await newSystem({
 		name: tuGroup.name,
+		image: await getImage(deriveHash(tuGroup.avatar, discordId)),
 		description: tuGroup.description || undefined,
-		type: "member",
-		viewInLists: false,
+		viewInLists: true,
 		isArchived: false,
+		isPinned: false
 	});
 
 	if(!transactionSucceeded(result))
 		throw new Error(`Could not add Tupperbox tag: ${result.err.message}`);
 
-	tagMapping.set(tuGroup.id, result.detail);
+	systemMapping.set(tuGroup.id, result.detail);
 }
 
-async function member(tuMember: TupperboxMember){
+async function member(tuMember: TupperboxMember, discordId?: string){
 	const result = await newMember({
 		name: tuMember.name,
 		system: appConfig.defaultSystem,
-		image: await getImage(tuMember.avatar_url), // await getImage(deriveHash(<DISCORD ID GOES HERE>, tuMember.avatar))
-		//cover: await getImage(deriveHash(<DISCORD ID GOES HERE>, tuMember.banner), 1024),
+		image: await getImage(deriveHash(tuMember.avatar, discordId) || tuMember.avatar_url),
+		cover: await getImage(deriveHash(tuMember.banner, discordId), 1024),
 		description: tuMember.description || undefined,
 		pronouns: tuMember.pronouns || undefined,
 		isArchived: false,
@@ -60,8 +61,8 @@ export async function importTupperBox(){
 		});
 		if (!path) throw new Error("no path");
 
-		const tagMapping = new Map<number, string>();
-		const memberWantsTags = new Map<string, number | undefined>();
+		const subsystemMapping = new Map<number, string>();
+		const memberWantsSubsystem = new Map<string, number | undefined>();
 
 		// WIPE AMPERSAND
 		await clearAllDatabase();
@@ -85,29 +86,30 @@ export async function importTupperBox(){
 		for await (const { path, value } of streamToIterable(jsonStream)){
 			switch(path[0]){
 				case "groups": {
-					await tag(value as unknown as TupperboxGroup, tagMapping);
+					await subsystems(value as unknown as TupperboxGroup, subsystemMapping);
 					break;
 				}
 				case "tuppers": {
-					const [uuid, tbTag] = await member(value as unknown as TupperboxMember);
-					memberWantsTags.set(uuid, tbTag);
+					const [uuid, tbGroup] = await member(value as unknown as TupperboxMember);
+					memberWantsSubsystem.set(uuid, tbGroup);
 					break;
 				}
 			}
 		}
 
 		// Assign tags to members at a later step
-		for(const [uuid, tag] of memberWantsTags){
-			if(!tag) continue;
-			const _tagId = tagMapping.get(tag);
-			if (!_tagId) continue;
+		for(const [uuid, tbGroup] of memberWantsSubsystem){
+			if(!tbGroup) continue;
+			const _systemId = subsystemMapping.get(tbGroup);
+			if (!_systemId) continue;
 
 			const result = await updateMember({
 				uuid,
-				tags: [_tagId]
+				system: _systemId
 			});
+
 			if (!transactionSucceeded(result))
-				throw new Error(`Could not remap Tupperbox member -> tag: ${result.err.message}`);
+				throw new Error(`Could not remap Tupperbox member -> system: ${result.err.message}`);
 		}
 	}catch(e){
 		console.error(e);
