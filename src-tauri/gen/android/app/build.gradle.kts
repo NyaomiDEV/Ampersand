326cea62c80486732a7e486cc96441fccedb8100
Naomi Calabretta <nyaomicalabretta@gmail.com>
2026-09-07 09:54:29 +0200

    version 0.3.1-rc3

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -111,10 +111,10 @@ rust {
 }
 
 dependencies {
-    implementation("androidx.webkit:webkit:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.12.2")
-    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.webkit:webkit:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.13.0")
+    implementation("com.google.android.material:material:1.14.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
